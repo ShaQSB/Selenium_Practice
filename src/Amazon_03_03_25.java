bf71c51e096da66a4ce10729fb2d4394843391e8
ShaQB <shaqb2022@gmail.com>
2025-03-03 17:46:21 +0530

    the files are committed

---
+++ src/Amazon_03_03_25.java
@@ -0,0 +1,15 @@
+public class Amazon_03_03_25 {
+
+    // Find the Page title and then display it in console
+
+    // Scroll down to bottom and find the web page
+    // 1996-2025, Amazon.com, Inc. or its affiliates xpath and print it in console
+
+    // HOVER onto the En language option and change language of the webpage
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
