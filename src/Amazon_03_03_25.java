1074a448c7b7571965df87e09d74dbe8cf14ef29
ShaQB <shaqb2022@gmail.com>
2025-03-03 18:21:57 +0530

    Added

--- src/Amazon_03_03_25.java
+++ src/Amazon_03_03_25.java
@@ -1,3 +1,15 @@
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
 public class Amazon_03_03_25 {
 
     // Find the Page title and then display it in console
@@ -8,7 +20,60 @@ public class Amazon_03_03_25 {
     // HOVER onto the En language option and change language of the webpage
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver .manage().window().maximize();
+
+        driver.get("https://www.amazon.in/");
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.getTitle();
+
+        WebElement scrolToBot= driver.findElement(By.xpath("//span[starts-with(text(),'© ')]"));
+
+        JavascriptExecutor je=(JavascriptExecutor) driver;
+
+        je.executeScript("arguments[0].scrollIntoView(true);",scrolToBot);
+
+        String footertext= String.valueOf(scrolToBot);
+
+        System.out.println(footertext);
+
+        Thread.sleep(5000);
+
+
+        je.executeScript("scroll(0,0)");
+
+        System.out.println("Scrolled to Top");
+
+        WebElement lang=driver.findElement(By.xpath("//div[text()='EN']"));
+
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(lang).perform();
+
+        Thread.sleep(5000);
+
+        WebElement langselect= driver.findElement(By.xpath("//span[@style='unicode-bidi:isolate; direction:ltr;'][text()='ಕನ್ನಡ']"));
+
+        langselect.click();
+
+        Thread.sleep(3000);
+
+        WebElement loginhover=driver.findElement(By.cssSelector("a[data-nav-role='signin']"));
+
+        actions.moveToElement(loginhover).perform();
+
+        Thread.sleep(5000);
+
+
+        WebElement signin=driver.findElement(By.cssSelector("span[class='nav-action-inner']"));
+
+        signin.click();
 
 
     }
